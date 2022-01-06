8ce18da2776a322020edf4ba6724e7019079f98b
Jordan Devaney <jordandevaney28@gmail.com>
2022-01-06 12:42:26 -0500

    add about and checkout page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,37 +1,56 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+	return (
+		<main>
+			<PageHero title="About" />
+			<Wrapper className="page section section-center">
+				<img src={aboutImg} alt=" Nice Desk" />
+				<article>
+					<div className="title">
+						<h2>Our Story</h2>
+						<div className="underline" />
+					</div>
+					<p>
+						Lorum Ipsum Deler sit amet. Lorum Ipsum Deler sit amet. Lorum Ipsum
+						Deler sit amet Lorum Ipsum Deler sit amet Lorum Ipsum Deler sit amet
+						Lorum Ipsum Deler sit amet Lorum Ipsum Deler sit amet Lorum Ipsum
+						Deler sit amet Lorum Ipsum Deler sit amet Lorum Ipsum Deler sit amet
+					</p>
+				</article>
+			</Wrapper>
+		</main>
+	);
+};
 
 const Wrapper = styled.section`
-  display: grid;
-  gap: 4rem;
-  img {
-    width: 100%;
-    display: block;
-    border-radius: var(--radius);
-    height: 500px;
-    object-fit: cover;
-  }
-  p {
-    line-height: 2;
-    max-width: 45em;
-    margin: 0 auto;
-    margin-top: 2rem;
-    color: var(--clr-grey-5);
-  }
-  .title {
-    text-align: left;
-  }
-  .underline {
-    margin-left: 0;
-  }
-  @media (min-width: 992px) {
-    grid-template-columns: 1fr 1fr;
-  }
-`
-export default AboutPage
+	display: grid;
+	gap: 4rem;
+	img {
+		width: 100%;
+		display: block;
+		border-radius: var(--radius);
+		height: 500px;
+		object-fit: cover;
+	}
+	p {
+		line-height: 2;
+		max-width: 45em;
+		margin: 0 auto;
+		margin-top: 2rem;
+		color: var(--clr-grey-5);
+	}
+	.title {
+		text-align: left;
+	}
+	.underline {
+		margin-left: 0;
+	}
+	@media (min-width: 992px) {
+		grid-template-columns: 1fr 1fr;
+	}
+`;
+export default AboutPage;
