f7a97e4e95a67f4a771421427af8b195dfb233ae
Jordan Devaney <jordandevaney28@gmail.com>
2022-01-06 21:24:53 -0500

    fetch products to add to context API

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -15,6 +15,10 @@ import {
 
 const initialState = {
 	isSidebarOpen: false,
+	products_loading: false,
+	products_error: false,
+	products: [],
+	featured_products: [],
 };
 
 const ProductsContext = React.createContext();
@@ -27,6 +31,20 @@ export const ProductsProvider = ({ children }) => {
 	const closeSidebar = () => {
 		dispatch({ type: SIDEBAR_CLOSE });
 	};
+
+	const fetchProducts = async (url) => {
+		dispatch({ type: GET_PRODUCTS_BEGIN });
+		try {
+			const response = await axios.get(url);
+			dispatch({ type: GET_PRODUCTS_SUCCESS, payload: response.data });
+		} catch (error) {
+			dispatch({ type: GET_PRODUCTS_ERROR });
+		}
+	};
+
+	useEffect(() => {
+		fetchProducts(url);
+	}, []);
 	return (
 		<ProductsContext.Provider value={{ closeSidebar, openSidebar, ...state }}>
 			{children}
